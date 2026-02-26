6955ebe6cde1db09c7eb9a778597f5c45d64f921
edhoblyn <edhoblyn@gmail.com>
2026-02-26 15:23:27 +0000

    added a navbar

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -20,30 +20,28 @@ class ListsController < ApplicationController
     end
   end
 
+  def destroy
+    @list = List.find(params[:id])
+    @list.destroy
+    redirect_to list_path, status: :see_other
+  end
+
+  def edit
+    @list = List.find(params[:id])
+  end
+
+  def update
+    @list = List.find(params[:id])
+    if @list.update(list_params)
+      redirect_to list_path(@list)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
    private
 
    def list_params
      params.require(:list).permit(:name)
    end
 end
-
-
-
-#   def edit
-#     @flat = Flat.find(params[:id])
-#   end
-
-#   def update
-#     @flat = Flat.find(params[:id])
-#     if @flat.update(flat_params)
-#       redirect_to flat_path(@flat)
-#     else
-#       render :edit, status: :unprocessable_entity
-#     end
-#   end
-
-#   def destroy
-#     @flat = Flat.find(params[:id])
-#     @flat.destroy
-#     redirect_to flats_path, status: :see_other
-#   end
