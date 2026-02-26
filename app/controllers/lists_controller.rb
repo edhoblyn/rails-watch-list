2bc42e368087be52528f4e6666d3a2be882ba3d8
edhoblyn <edhoblyn@gmail.com>
2026-02-26 13:58:54 +0000

    added views

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -20,8 +20,6 @@ class ListsController < ApplicationController
     end
   end
 
-
-
    private
 
    def list_params
@@ -49,10 +47,3 @@ end
 #     @flat.destroy
 #     redirect_to flats_path, status: :see_other
 #   end
-
-#   private
-
-#   def flat_params
-#     params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests)
-#   end
-# end
